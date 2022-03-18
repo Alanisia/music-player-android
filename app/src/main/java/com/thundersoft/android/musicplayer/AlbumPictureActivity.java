36f7cec471b745250b64af21018d4c7e2af8c939
Alanisia <2478196682@qq.com>
2022-03-18 21:03:31 +0800

    Create project

---
+++ app/src/main/java/com/thundersoft/android/musicplayer/AlbumPictureActivity.java
@@ -0,0 +1,14 @@
+package com.thundersoft.android.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlbumPictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album_picture);
+    }
+}
